c795d39e64cee2b76b4ffeca0ba269ee63c404cb
felipesantinho <felipeaugustosantinho@gmail.com>
2022-08-30 15:18:59 -0300

    atualizacao

---
+++ aula12/ex014.js
@@ -0,0 +1,13 @@
+var sexo = 'M'
+
+switch (sexo){
+  case 'M':
+    console.log('Seu sexo é masculino')
+    break
+  case 'F':
+    console.log('Seu sexo é feminino')
+    break
+  default:
+    console.log('Sem sexo definido')
+    break
+}
